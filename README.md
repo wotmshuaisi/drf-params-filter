f01215aa1bea8b6eaf93d4cc6a0787ac85d2ecc2
wotmshuaisi <16408269+wotmshuaisi@users.noreply.github.com>
2018-03-08 15:00:52 +0800

    init version

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # request_params_filter
-a django plugin to filter record with GET params
+
+[![forthebadge](http://forthebadge.com/images/badges/built-by-codebabes.svg)](http://forthebadge.com)  
+
+to filter the database record with GET params is easily, but trouble, so i wrote this plugin for DRF3,  hope it works for you
+
+# Getting Started
+
+to filter record you need to create a class base on BaseFilter, then write a dict `fields` with format {'params': 'django_orm_conditions'}
+## Example
+
+- project/api/filter.py
+
+```python
+from request_params_filter.filter import BaseFilter
+
+class CustomFilter(BaseFilter):
+    fields = {
+        'date': 'datetime_startswith',
+        'name': 'name',
+        'max_amount': 'amount__gte',
+        'min_amount': 'amount__lte'
+    }
+```
+
+- project/api/viewsets/test_viewset.py
+
+```python
+from test.models import Test
+from project.api.filter import CustomFilter
+
+class TestViewSet(viewset.Viewset):
+    def list(self, request):
+        queryset = Test.objects.filter(user=request.user).all()
+        filter_obj = CustomFilter(request.query_params, queryset)
+        # normal
+        queryset = filter_obj.conditions_queryset()
+        # to skip some params
+        # queryset = filter_obj.conditions_queryset(['max_amount', 'min_amount'])        
+        return Response(queryset)
+```
+
+
+## Depends
+
+- django.db.models.Q()
---
+++ filter.py
@@ -0,0 +1,49 @@
+from django.db.models import Q
+
+
+class BaseFilter(object):
+    # fields dict, params_name: conditions
+    fields = {}
+
+    def __init__(self, query_params, queryset):
+        self.q_obj = Q()
+        self.query_params = query_params
+        self.queryset = queryset
+
+    def __filter_field(self, field_name, field_val):
+        self.q_obj.children.append((field_name, field_val))
+
+    def __generate_queryset(self,):
+        if self.q_obj.children.__len__() > 1:
+            self.q_obj.connector = 'and'
+        return self.queryset.filter(self.q_obj)
+
+    def __params_is_valid(self, ):
+        """
+        Judgment the params is valid or not
+        """
+        if not self.query_params:
+            return False
+        for item in self.fields.keys():
+            if item in self.query_params:
+                return True
+        else:
+            return False
+
+    def conditions_queryset(self, exclude_params=[]):
+        # if params not valid, return origin querset
+        if not self.__params_is_valid():
+            return self.queryset
+        # auto add conditions from fields dict
+        for key, condition in self.fields.items():
+            # pass unknow params
+            if not key in self.query_params:
+                continue
+            # pass exclude params
+            if key in exclude_params:
+                continue
+            self.__filter_field(
+                condition,
+                self.query_params.get(key))
+        # return a conditional queryset
+        return self.__generate_queryset()
